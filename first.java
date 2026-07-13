adf73e12b2177d74bc48f7c03413fd99b9e23029
yeeshu04 <yeeshumathur0404@gmail.com>
2026-07-13 15:41:55 +0530

    Add files via upload
    first code in java language

---
+++ first.java
@@ -0,0 +1,16 @@
+public class first {
+    public static void main(String[] args) {
+
+        // Here is the starting of Java program
+        /*
+        h
+        h
+        h
+         */
+
+        System.out.println("Hello World");
+        System.out.print("This is our first java code\n");
+        System.out.println("Here we will complete the java");
+
+    }
+}
