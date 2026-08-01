7bbd6fc4080885cf7ec1b02b6b0626910cf5a824
yeeshu04 <yeeshumathur0404@gmail.com>
2026-08-01 16:37:27 +0530

    Add files via upload
    # 🐾 Animal Class
    
    A Java parent class that extends the `Organism` class and adds common animal behavior through an `eat()` method. It serves as the base class for different animal types.
    
    ## Features
    - Inherits from Organism
    - Common animal behavior
    - Base class for Dog and Cat
    
    ## Concepts Practiced
    - Inheritance
    - extends Keyword
    - Method Inheritance
    - Superclass

---
+++ day12/Inheritance/animal1.java
@@ -0,0 +1,14 @@
+package day12.Inheritance;
+
+//this is a parent class
+
+//To inherit all the attributes & methods from another class
+//after the class name we use extends keyword then specify the parent class
+public class animal1 extends organism {
+
+
+    void eat() {
+        System.out.println("animals eat");
+    }
+
+}
