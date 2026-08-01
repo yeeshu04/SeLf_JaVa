9dc4c4462deab05c9dae6a50cd6f70563c55d652
yeeshu04 <yeeshumathur0404@gmail.com>
2026-08-01 16:38:22 +0530

    Add files via upload
    # 🌿 Plant Class
    
    A Java class that extends the `Organism` class and demonstrates inheritance by adding plant-specific behavior.
    
    ## Features
    - Inherits from Organism
    - Plant-specific method
    - Shared properties through inheritance
    
    ## Concepts Practiced
    - Inheritance
    - Child Classes
    - Method Creation
    - Object-Oriented Programming

---
+++ day12/Inheritance/plant.java
@@ -0,0 +1,10 @@
+package day12.Inheritance;
+
+//To inherit all the attributes & methods from another class
+//after the class name we use extends keyword then specify the parent class
+public class plant extends organism {
+
+    void photosynthesis() {
+        System.out.println("The plant absorbs sunlight");
+    }
+}
